e55d4c019d8af40db81eb83fe9a994d8d4893b5a
anandhu-co-in <mail.anandhu@gmail.com>
2021-06-02 21:53:06 +0530

    preapred deployment branch

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const connectDB=require('./config/db')
 const express = require("express")
+const path=require('path');
 
 const app=express()
 connectDB()
@@ -9,9 +10,9 @@ connectDB()
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.get("/",(req,res)=>{
-    res.send("worked");
-})
+// app.get("/",(req,res)=>{
+//     res.send("worked");
+// })
 
 
 //Adding the routes
@@ -19,6 +20,17 @@ app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/posts',require('./routes/api/posts'));
+
+
+//Serve static assets in production
+if(process.env.NODE_ENV==='production'){
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 
 app.listen(process.env.PORT||3001,()=>console.log("Server started"))
